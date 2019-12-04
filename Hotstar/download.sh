849c2acf981c5724204a5ec4fb2b89159cc9c8ec
Harish Sridharan <harish.sridhar@gmail.com>
2019-12-04 11:56:04 +0530

    Added 2nd episode for testing

--- Hotstar/download.sh
+++ Hotstar/download.sh
@@ -4,6 +4,18 @@
 
 target_rep=$1
 max_fragment_size=$2
+base_url=$3
+
+# constants
+header_referer="referer: https://www.hotstar.com/"
+header_user_agent="user-agent: Mozilla/5  AppleWebKit/537 Chrome/78 Safari/537"
+
+
+if [ "x$base_url" == "x" ] 
+then
+echo "Missing base URL"
+exit 0
+fi
 
 if [ "x$target_rep" == "x" ] 
 then
@@ -39,25 +51,26 @@ echo "Directories made : $audio_rep"
 
 
 # Video init file
-video_init_url="https://hses.akamaized.net/videos/hbo/goths/2/5605640392/1770005122/1558678315204/f516d290292bb2ed2b55978a9ae7bd75/$video_rep/init.mp4"
-curl --url "$video_init_url" --output "$video_rep/init.mp4" -H "referer: https://www.hotstar.com/" -H "user-agent: Mozilla/5  AppleWebKit/537 Chrome/78 Safari/537"
+video_init_url="$base_url/$video_rep/init.mp4"
+
+curl --url "$video_init_url" --output "$video_rep/init.mp4" -H "$header_referer" -H "$header_user_agent"
 echo "Video Init downloaded : $video_rep/init.mp4"
 
 # Audio init file
-audio_init_url="https://hses.akamaized.net/videos/hbo/goths/2/5605640392/1770005122/1558678315204/f516d290292bb2ed2b55978a9ae7bd75/$audio_rep/init.mp4"
-curl --url "$audio_init_url" --output "$audio_rep/init.mp4" -H "referer: https://www.hotstar.com/" -H "user-agent: Mozilla/5  AppleWebKit/537 Chrome/78 Safari/537"
+audio_init_url="$base_url/$audio_rep/init.mp4"
+curl --url "$audio_init_url" --output "$audio_rep/init.mp4" -H "$header_referer" -H "$header_user_agent"
 echo "Audio Init downloaded : $audio_rep/init.mp4"
 
 for number in $(seq 1 $max_fragment_size);
 do
  #video fragments
- url="https://hses.akamaized.net/videos/hbo/goths/2/5605640392/1770005122/1558678315204/f516d290292bb2ed2b55978a9ae7bd75/$video_rep/seg-$number.m4s"
- curl --url "$url" --output "$video_rep/seg-$number.m4s" -H "referer: https://www.hotstar.com/" -H "user-agent: Mozilla/5  AppleWebKit/537 Chrome/78 Safari/537"
+ url="$base_url/$video_rep/seg-$number.m4s"
+ curl --url "$url" --output "$video_rep/seg-$number.m4s" -H "$header_referer" -H "$header_user_agent"
  echo "Video downloaded : $video_rep/seg-$number.m4s"
 
  #audio fragments
- url="https://hses.akamaized.net/videos/hbo/goths/2/5605640392/1770005122/1558678315204/f516d290292bb2ed2b55978a9ae7bd75/$audio_rep/seg-$number.m4s"
- curl --url "$url" --output "$audio_rep/seg-$number.m4s" -H "referer: https://www.hotstar.com/" -H "user-agent: Mozilla/5  AppleWebKit/537 Chrome/78 Safari/537"
+ url="$base_url/$audio_rep/seg-$number.m4s"
+ curl --url "$url" --output "$audio_rep/seg-$number.m4s" -H "$header_referer" -H "$header_user_agent"
  echo "Audio downloaded : $audio_rep/seg-$number.m4s"
 done
 
